5e3cd29b9efa2a1d1f5d2c38fa2d19322964f265
Teja Sri <ptejasri2205@gmail.com>
2026-06-10 23:31:38 +0530

    Update BinaryTreeInorderTraversal.java

--- Trees/BinaryTreeInorderTraversal.java
+++ Trees/BinaryTreeInorderTraversal.java
@@ -1,5 +1,8 @@
 //Problem : Binary Tree Inorder Traversal
 
+// Link :
+// https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
 //Appraoch 1 : Recursive Traversal (Brute Force)
 //Time Complexity : O(n) → every node visited once
 //Space Complexity : O(h) → recursion stack height
